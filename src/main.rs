1e124af434734aa8946e3729be5a5ff15a00c1ea
Philip Woods <elzair@pop-os.localdomain>
2019-10-02 16:08:28 -0400

    Generated random dungeon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::cmp;
+
+use rand::Rng;
+
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -69,13 +73,124 @@ impl Tile {
 
 type Map = Vec<Vec<Tile>>;
 
-fn make_map() -> Map {
-    // fill map with "unblocked" tiles
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+// Dungeon Generator Parameters
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
-    // place two pillars to test the map
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+/// A rectangle on the map, used to characterize a room.
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        // Returns true if this rectangle intersects with another one
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    // Go through the tiles in the rectangle and make them passable
+    for x in (room.x1+1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // horizontal tunnel `min()` and `max()` are used in case `x1 > x2`
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // horizontal tunnel `min()` and `max()` are used in case `y1 > y2`
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn make_map(player: &mut Object) -> Map {
+    // fill map with "blocked" tiles
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // Create rooms
+    let mut rooms = vec![];
+
+    for _ in 0..MAX_ROOMS {
+        // Random width and height
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        // Random position without going out of the boundaries of the map
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        // Run through the other rooms and see if they intersect with this one
+        let failed = rooms.iter().any(|other_room| new_room.intersects_with(other_room));
+
+        if !failed {
+            // The room is valid if there are no intersections
+
+            // "Paint" it to the map's tiles
+            create_room(new_room, &mut map);
+
+            // Center coordinates of the new room
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                // Start player at first created room
+                player.x = new_x;
+                player.y = new_y;
+            } else {
+                // Connect new room to previous room with a tunnel
+
+                // Get center coordinates of the previous room
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // Toss a coin (random bool value -- either true or false)
+                if rand::random() {
+                    // First move horizontally, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    // First move vertically, then horizontally
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            // Finally, append the new room to `rooms`
+            rooms.push(new_room);
+        }
+    }
+
 
     map
 }
@@ -162,7 +277,7 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
     // Create object representing the player
-    let player = Object::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', WHITE);
+    let player = Object::new(0, 0, '@', WHITE);
 
     // Create an NPC
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
@@ -172,7 +287,7 @@ fn main() {
 
     let game = Game {
         // Generate map (at this point it is not drawn to the screen)
-        map: make_map(),
+        map: make_map(&mut objects[0]),
     };
 
     while !tcod.root.window_closed() {
